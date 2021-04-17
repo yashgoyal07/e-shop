72cc2aa1e0fc6c1de8d839b661d388738fb44be5
Yash Goyal <yashgoyalcs@gmail.com>
2021-04-17 12:47:57 +0530

    data

---
+++ src/products.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: eshop
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (3,'Samsung','It is a Japanese mobile',10000,'mobile',5,1210,600,'https://i.gadgets360cdn.com/products/large/1555507135_635_samsung_galaxy_a60.jpg'),(6,'Redmi 7','It is an Chinese Mobile.',9000,'mobile',8,4568,952,'https://i01.appmifile.com/webfile/globalimg/in/cms/1AF83159-0D7D-8988-7A33-644130D67669.jpg'),(7,'Realme','It is a chinese mobile',15000,'mobile',9,152,65,'https://static.digit.in/default/26af91b06c0e70e2de1d0ea6dd5aca991b81bb28.jpeg?tr=1200'),(8,'Nokia 3310','It is an american mobile',1400,'mobile',8,5468,4000,'https://cdn.shopify.com/s/files/1/2505/6374/products/nokia_3310-SSIM-red.png?v=1517828072'),(9,'Oppo A11','It is a Chinese mobile',12000,'mobile',5,456,123,'https://www.gizmochina.com/wp-content/uploads/2020/10/aaaa-500x500.jpg'),(10,'Hp Pavilion','It is an american laptop',55000,'laptop',1,1,10000,'https://images.unsplash.com/photo-1588872657578-7efd1f1555ed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'),(11,'HMT ','It is an Indian Watch',800,'watch',19,200,140,'https://4.imimg.com/data4/IM/VW/ANDROID-50613582/product-500x500.jpeg'),(12,'mac air','apple mac',300000,'laptop',1,500,50000,'https://yashretail.com/wp-content/uploads/2020/11/apple-na-thin-and-light-laptop-original-imafe6f78hur4jbh.jpeg'),(13,'Titan Watch','It is an Indian Watch',20000,'watch',9,250,1333,'https://staticimg.titan.co.in/Titan/Catalog/9162YL01_2.jpg');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-17 12:36:58
